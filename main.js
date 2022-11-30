f56f751f2513a534d79d70309691076b9290caad
Theofedericiolivieri <112435873+Theofedericiolivieri@users.noreply.github.com>
2022-11-30 18:58:52 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,18 @@ function start(){
     Textbox.innerHTML="";
     recognition.start();
 }
+recognition.onresult=function(event){
+    console.log(event);
+    var Content= event.results[0][0].transcript;
+    Textbox.innerHTML=Content;
+    console.log(Content);
+    if(Content=="tire minha selfie"){
+        console.log("tirando selfie---");
+        speak();
+    }
+}
 function speak(){
-    var synth= window.SpeechSynthesis;
+    var synth = window.speechSynthesis;
     speakData= "Tirando sua selfie em 5 segundos";
     var utterThis= new SpeechSynthesisUtterance(speakData);
     synth.speak(utterThis);
@@ -23,3 +33,14 @@ Webcam.set({
     image_format: 'jpeg',
     jpeg_quality:90
 });
+function takeSelfie(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfieImage" src="'+data_uri+'"/>';
+    });
+}
+function Save(){
+    link= document.getElementById("link");
+    image=document.getElementById("selfieImage").src;
+    link.href=image;
+    link.click();
+}
